c549e5c217c0e8002fae84bceaf36bb901b6d7be
Ashif <theashifkhan@gmail.com>
2020-01-24 17:10:17 +0530

    Try Catch Added

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,25 +3,31 @@ using namespace std;
 
 int permutation(int n, int r)
 {
-	if(r==0){
+	if(n<0 || r<0){
+		throw "Permutation Is Valid For Positive Number";
+	}
+	if(r==0){	//base condition 
 		return 1;
 	}else{
 		return n*permutation(n-1,r-1);
 	}
   	return 0;
-}
+}//permutation ends here
 
 int combination(int n, int r)
 {
-	if(r==0){
+	if(n<0 || r<0){
+		throw "Combination Is Valid For Positive Number";
+	}
+	if(r==0){	//base condition
 		return 1;
 	}else{
+		//return ((n)*combination(n-1,r-1))/r;
 		return (n/r)*combination(n-1,r-1);
 	}
-  	return 0;
-}
+}//combination ends here
 
-int main()
+int main()	//driver code
 {
 	int n,r;
 	cout<<"Enter The Value Of n And r to Calculate p(n,r) and c(n,r):-\n";
@@ -29,7 +35,16 @@ int main()
 	cin>>n;
 	cout<<"r : ";
 	cin>>r;
-	cout<<"Permutation Of  p("<<n<<","<<r<<")"<<" : "<<permutation(n,r);
-	cout<<"\nCombination Of  c("<<n<<","<<r<<")"<<" : "<<combination(n,r);
+	int result_p,result_c;
+	try{
+		result_p = permutation(n,r);
+		result_c = combination(n,r);
+		cout<<"Permutation Of  p("<<n<<","<<r<<")"<<" : "<<result_p;
+		cout<<"\nCombination Of  c("<<n<<","<<r<<")"<<" : "<<result_c;
+	}
+	catch(const char* str){
+		cout<<str;
+	}
+
   	return 0;
-}
+}//main ends here
