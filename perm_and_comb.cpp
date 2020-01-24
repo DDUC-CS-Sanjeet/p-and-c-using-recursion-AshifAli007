3917be671db43acf588b1db737570012f3e5227f
Ashif <theashifkhan@gmail.com>
2020-01-24 16:33:34 +0530

    Combination function completed

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -13,18 +13,23 @@ int permutation(int n, int r)
 
 int combination(int n, int r)
 {
-	
+	if(r==0){
+		return 1;
+	}else{
+		return (n/r)*combination(n-1,r-1);
+	}
   	return 0;
 }
 
 int main()
 {
 	int n,r;
-	cout<<"Enter The Value Of n And r to Calculate p(n,r) :-\n";
+	cout<<"Enter The Value Of n And r to Calculate p(n,r) and c(n,r):-\n";
 	cout<<"n : ";
 	cin>>n;
 	cout<<"r : ";
 	cin>>r;
 	cout<<"Permutation Of  p("<<n<<","<<r<<")"<<" : "<<permutation(n,r);
+	cout<<"\nCombination Of  c("<<n<<","<<r<<")"<<" : "<<combination(n,r);
   	return 0;
 }
