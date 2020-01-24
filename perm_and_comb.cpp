c1f6167d308f14113bffc044ba2a5572844b7083
Ashif <theashifkhan@gmail.com>
2020-01-24 16:14:02 +0530

    Permutation Completed

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,28 @@ using namespace std;
 
 int permutation(int n, int r)
 {
-  return 0;
+	if(r==0){
+		return 1;
+	}else{
+		return n*permutation(n-1,r-1);
+	}
+  	return 0;
 }
 
 int combination(int n, int r)
 {
-  return 0;
+	
+  	return 0;
 }
 
 int main()
 {
-  return 0;
+	int n,r;
+	cout<<"Enter The Value Of n And r to Calculate p(n,r) :-\n";
+	cout<<"n : ";
+	cin>>n;
+	cout<<"r : ";
+	cin>>r;
+	cout<<"Permutation Of  p("<<n<<","<<r<<")"<<" : "<<permutation(n,r);
+  	return 0;
 }
